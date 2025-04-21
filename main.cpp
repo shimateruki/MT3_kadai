a167a829dc5713dfa876b341413a8a1e84b112e0
shimateruki <k024g0032@g.neec.ac.jp>
2025-04-21 10:34:11 +0900

    初期化

--- main.cpp
+++ main.cpp
@@ -1,195 +1,7 @@
 #include <Novice.h>
 const char kWindowTitle[] = "LC1C_09_シマ_テルキ_タイトル";
 
-struct matrix4x4
-{
-	float m[4][4];
-};
 
-//行列の計算
-matrix4x4 Add(const matrix4x4& m1, const  matrix4x4& m2)
-{
-	matrix4x4 result = {};
-
-	result.m[0][0] = m1.m[0][0] + m2.m[0][0];
-	result.m[0][1] = m1.m[0][1] + m2.m[0][1];
-	result.m[0][2] = m1.m[0][2] + m2.m[0][2];
-	result.m[0][3] = m1.m[0][3] + m2.m[0][3];
-
-	result.m[1][0] = m1.m[1][0] + m2.m[1][0];
-	result.m[1][1] = m1.m[1][1] + m2.m[1][1];
-	result.m[1][2] = m1.m[1][2] + m2.m[1][2];
-	result.m[1][3] = m1.m[1][3] + m2.m[1][3];
-
-
-	result.m[2][0] = m1.m[2][0] + m2.m[2][0];
-	result.m[2][1] = m1.m[2][1] + m2.m[2][1];
-	result.m[2][2] = m1.m[2][2] + m2.m[2][2];
-	result.m[2][3] = m1.m[2][3] + m2.m[2][3];
-
-
-	result.m[3][0] = m1.m[3][0] + m2.m[3][0];
-	result.m[3][1] = m1.m[3][1] + m2.m[3][1];
-	result.m[3][2] = m1.m[3][2] + m2.m[3][2];
-	result.m[3][3] = m1.m[3][3] + m2.m[3][3];
-
-	return result;
-
-}
-//行列の減法
-matrix4x4 subtract(const matrix4x4& m1, const  matrix4x4& m2)
-{
-	matrix4x4 result = {};
-
-	result.m[0][0] = m1.m[0][0] - m2.m[0][0];
-	result.m[0][1] = m1.m[0][1] - m2.m[0][1];
-	result.m[0][2] = m1.m[0][2] - m2.m[0][2];
-	result.m[0][3] = m1.m[0][3] - m2.m[0][3];
-
-	result.m[1][0] = m1.m[1][0] - m2.m[1][0];
-	result.m[1][1] = m1.m[1][1] - m2.m[1][1];
-	result.m[1][2] = m1.m[1][2] - m2.m[1][2];
-	result.m[1][3] = m1.m[1][3] - m2.m[1][3];
-
-
-	result.m[2][0] = m1.m[2][0] - m2.m[2][0];
-	result.m[2][1] = m1.m[2][1] - m2.m[2][1];
-	result.m[2][2] = m1.m[2][2] - m2.m[2][2];
-	result.m[2][3] = m1.m[2][3] - m2.m[2][3];
-
-
-	result.m[3][0] = m1.m[3][0] - m2.m[3][0];
-	result.m[3][1] = m1.m[3][1] - m2.m[3][1];
-	result.m[3][2] = m1.m[3][2] - m2.m[3][2];
-	result.m[3][3] = m1.m[3][3] - m2.m[3][3];
-
-	return result;
-}
-//行列の積
-matrix4x4 Multiply(const matrix4x4& m1, const  matrix4x4& m2)
-{
-
-	matrix4x4 result = {};
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			result.m[i][j] =
-				m1.m[i][0] * m2.m[0][j] +
-				m1.m[i][1] * m2.m[1][j] +
-				m1.m[i][2] * m2.m[2][j] +
-				m1.m[i][3] * m2.m[3][j];
-		}
-	}
-	return result;
-
-}
-//逆行列
-matrix4x4 Inverse(const matrix4x4& m)
-{
-	matrix4x4 result = {};
-
-
-	float a11 = m.m[0][0], a12 = m.m[0][1], a13 = m.m[0][2], a14 = m.m[0][3];
-	float a21 = m.m[1][0], a22 = m.m[1][1], a23 = m.m[1][2], a24 = m.m[1][3];
-	float a31 = m.m[2][0], a32 = m.m[2][1], a33 = m.m[2][2], a34 = m.m[2][3];
-	float a41 = m.m[3][0], a42 = m.m[3][1], a43 = m.m[3][2], a44 = m.m[3][3];
-
-
-	float det = a11 * a22 * a33 * a44 + a11 * a23 * a34 * a42 + a11 * a24 * a32 * a43
-		- a11 * a24 * a33 * a42 - a11 * a23 * a32 * a44 - a11 * a22 * a34 * a43
-		- a12 * a21 * a33 * a44 - a13 * a21 * a34 * a42 - a14 * a21 * a32 * a43
-		+ a14 * a21 * a33 * a42 + a13 * a21 * a32 * a44 + a12 * a21 * a34 * a43
-		+ a12 * a23 * a31 * a44 + a13 * a24 * a31 * a42 + a14 * a22 * a31 * a43
-		- a14 * a23 * a31 * a42 - a13 * a22 * a31 * a44 - a12 * a24 * a31 * a43
-		- a12 * a23 * a34 * a41 - a13 * a24 * a32 * a41 - a14 * a22 * a33 * a41
-		+ a14 * a23 * a32 * a41 + a13 * a22 * a34 * a41 + a12 * a24 * a33 * a41;
-	if (det == 0.0f) {
-		// 逆行列が存在しない（行列式が0）
-		return result;
-	}
-
-	float invDet = 1.0f / det;
-
-	// 以下、各要素に対応する余因子を手動計算して代入（転置あり）
-
-	// 1行目
-	result.m[0][0] = (a22 * (a33 * a44 - a34 * a43) - a23 * (a32 * a44 - a34 * a42) + a24 * (a32 * a43 - a33 * a42)) * invDet;
-	result.m[0][1] = -(a12 * (a33 * a44 - a34 * a43) - a13 * (a32 * a44 - a34 * a42) + a14 * (a32 * a43 - a33 * a42)) * invDet;
-	result.m[0][2] = (a12 * (a23 * a44 - a24 * a43) - a13 * (a22 * a44 - a24 * a42) + a14 * (a22 * a43 - a23 * a42)) * invDet;
-	result.m[0][3] = -(a12 * (a23 * a34 - a24 * a33) - a13 * (a22 * a34 - a24 * a32) + a14 * (a22 * a33 - a23 * a32)) * invDet;
-
-	// 2行目
-	result.m[1][0] = -(a21 * (a33 * a44 - a34 * a43) - a23 * (a31 * a44 - a34 * a41) + a24 * (a31 * a43 - a33 * a41)) * invDet;
-	result.m[1][1] = (a11 * (a33 * a44 - a34 * a43) - a13 * (a31 * a44 - a34 * a41) + a14 * (a31 * a43 - a33 * a41)) * invDet;
-	result.m[1][2] = -(a11 * (a23 * a44 - a24 * a43) - a13 * (a21 * a44 - a24 * a41) + a14 * (a21 * a43 - a23 * a41)) * invDet;
-	result.m[1][3] = (a11 * (a23 * a34 - a24 * a33) - a13 * (a21 * a34 - a24 * a31) + a14 * (a21 * a33 - a23 * a31)) * invDet;
-
-	// 3行目
-	result.m[2][0] = (a21 * (a32 * a44 - a34 * a42) - a22 * (a31 * a44 - a34 * a41) + a24 * (a31 * a42 - a32 * a41)) * invDet;
-	result.m[2][1] = -(a11 * (a32 * a44 - a34 * a42) - a12 * (a31 * a44 - a34 * a41) + a14 * (a31 * a42 - a32 * a41)) * invDet;
-	result.m[2][2] = (a11 * (a22 * a44 - a24 * a42) - a12 * (a21 * a44 - a24 * a41) + a14 * (a21 * a42 - a22 * a41)) * invDet;
-	result.m[2][3] = -(a11 * (a22 * a34 - a24 * a32) - a12 * (a21 * a34 - a24 * a31) + a14 * (a21 * a32 - a22 * a31)) * invDet;
-
-	// 4行目
-	result.m[3][0] = -(a21 * (a32 * a43 - a33 * a42) - a22 * (a31 * a43 - a33 * a41) + a23 * (a31 * a42 - a32 * a41)) * invDet;
-	result.m[3][1] = (a11 * (a32 * a43 - a33 * a42) - a12 * (a31 * a43 - a33 * a41) + a13 * (a31 * a42 - a32 * a41)) * invDet;
-	result.m[3][2] = -(a11 * (a22 * a43 - a23 * a42) - a12 * (a21 * a43 - a23 * a41) + a13 * (a21 * a42 - a22 * a41)) * invDet;
-	result.m[3][3] = (a11 * (a22 * a33 - a23 * a32) - a12 * (a21 * a33 - a23 * a31) + a13 * (a21 * a32 - a22 * a31)) * invDet;
-
-	return result;
-}
-	
-
-//転置行列
-matrix4x4 Transpose(const matrix4x4& m)
-{
-	matrix4x4 result = {};
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			result.m[i][j] = m.m[j][i];
-		}
-	}
-	return result;
-};
-//単位行列の作成
-matrix4x4 makeIdentity4x4()
-{
-	matrix4x4 result = {};
-	result.m[0][0] = 1.0f;
-	result.m[0][1] = 0.0f;
-	result.m[0][2] = 0.0f;
-	result.m[0][3] = 0.0f;
-
-	result.m[1][0] = 0.0f;
-	result.m[1][1] = 1.0f;
-	result.m[1][2] = 0.0f;
-	result.m[1][3] = 0.0f;
-
-	result.m[2][0] = 0.0f;
-	result.m[2][1] = 0.0f;
-	result.m[2][2] = 1.0f;
-	result.m[2][3] = 0.0f;
-
-	result.m[3][0] = 0.0f;
-	result.m[3][1] = 0.0f;
-	result.m[3][2] = 0.0f;
-	result.m[3][3] = 1.0f;
-	return result;
-};
-static const int kRowHeight = 20;
-static const int kColumnWidth = 60;
-void matrixScreenPrintf(int x, int y, const matrix4x4& matrix, const char* label)
-{
-	Novice::ScreenPrintf(x, y, "%s", label);
-	for (int row = 0; row < 4; ++row)
-	{
-		for (int column = 0;column < 4;++column)
-		{
-			Novice::ScreenPrintf(x + column * kColumnWidth, y + 20 + row * kRowHeight, "%6.02f", matrix.m[row][column]);
-		}
-	}
-}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -199,14 +11,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
-	matrix4x4 m1 = { 3.2f, 0.7f, 9.6f, 4.4f,
-					5.5f,1.3f, 7.8f, 2.1f,
-					6.9f, 8.0f, 2.6f, 1.0f,
-					0.5f, 7.2f, 5.1f, 3.3f };
-	matrix4x4 m2 = { 4.1f, 6.5f, 3.3f, 2.2f,
-	8.8f, 0.6f, 9.9f, 7.7f,
-	1.1f, 5.5f, 6.6f, 0.0f,
-	3.3f, 9.9f, 8.8f, 2.2f };
+
+	
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -221,14 +27,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		matrix4x4 resultAdd = Add(m1, m2);
-		matrix4x4 resultMultply = Multiply(m1, m2);
-		matrix4x4 resultSubtract = subtract(m1, m2);
-		matrix4x4 inverseM1 = Inverse(m1);
-		matrix4x4 inverseM2 = Inverse(m2);
-		matrix4x4 transposeM1 = Transpose(m1);
-		matrix4x4 transposeM2 = Transpose(m2);
-		matrix4x4 identity = makeIdentity4x4();
+		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -237,15 +36,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		matrixScreenPrintf(0, 0, resultAdd, "add");
-		matrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
-		matrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultply, "Multltply");
-		matrixScreenPrintf(0, kRowHeight * 5 * 3, inverseM1, "InverseM1");
-		matrixScreenPrintf(0, kRowHeight * 5 * 4, inverseM2, "InverseM2");
-		matrixScreenPrintf(kColumnWidth * 5, 0, transposeM1, "TransposeM1");
-		matrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5, transposeM2, "TransposeM2");
-		matrixScreenPrintf(kColumnWidth * 5, kRowHeight * 5 * 2, identity, "Identily");
-
+	
 		///
 		/// ↑描画処理ここまで
 		///
