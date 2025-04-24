fae9d99c74be82c2c250e6e3529dd7e2a5c0985c
shimateruki <k024g0032@g.neec.ac.jp>
2025-04-25 00:36:06 +0900

    まだ

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <Novice.h>
+#include <cmath>
+
 const char kWindowTitle[] = "LC1C_09_シマ_テルキ_タイトル";
+
+
+
+struct Matrix4x4
+{
+	float m[4][4];
+};
+
+struct Vector3
+{
+	float x;
+	float y;
+	float z;
+
+};
+
+//X軸回転行列
+Matrix4x4 MakeRotateXMatix(float radian)
+{
+
+}
+
+//Y軸回転行列
+
+Matrix4x4 MakeRotateYMatix(float radian)
+{
+
+}
+//Z軸回転行列
+Matrix4x4 MakeRotateZMatix(float radian)
+{
+
+}
+
+
+
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -11,6 +49,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+	Vector3 rotate = { 0.4f, 1.43f, -0.8f };
+	Matrix4x4 rotateXMatrix = MakeRotateXMatix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatix(rotate.y);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatix(rotate.z);
+
+	
+
 
 	
 
