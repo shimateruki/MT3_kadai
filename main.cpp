592706bcca0e5cba49b075c0fb255d606764cddd
shimateruki <k024g0032@g.neec.ac.jp>
2025-04-25 01:24:55 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -80,18 +80,24 @@ Matrix4x4 MakeRotateZMatix(float radian)
 	result.m[0][1] = sinf(radian);
 	result.m[0][2] = 0.0f;
 	result.m[0][3] = 0.0f;
+
 	result.m[1][0] = -sinf(radian);
 	result.m[1][1] = cosf(radian);
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
 	result.m[2][0] = 0.0f;
 	result.m[2][1] = 0.0f;
 	result.m[2][2] = 1.0f;
 	result.m[2][3] = 0.0f;
-	result.m[3][0] = translate.x;
-	result.m[3][1] = translate.y;
-	result.m[3][2] = translate.z;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
 	result.m[3][3] = 1.0f;
+
 	return result;
-};
+}
 
 //行列の積
 Matrix4x4 Multiply(const Matrix4x4& m1, const  Matrix4x4& m2)
@@ -179,10 +185,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 	
-			MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, 0, rotateXMatrix, "rotateXMatrix");
 		MatrixScreenPrintf(0, kRowHeight * 5, rotateYMatrix, "rotateYMatrix");
 		MatrixScreenPrintf(0, kRowHeight * 5 * 2, rotateZMatrix, "rotateZMatrix");
-		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, rotateXYZMatrix, "rotateXYZMatrix");
 
 	
 
